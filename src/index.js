aa3de3dcaf72529b3c05b6dbcf04d1157a50a374
Chris Padilla <cdpadilla42@gmail.com>
2020-01-24 16:47:02 -0600

    add full state

--- src/index.js
+++ src/index.js
@@ -25,16 +25,49 @@ const defaultCoffees = [
 
 class NewCoffeeForm extends React.Component {
   render() {
-    return <h1>Submit New Coffee</h1>;
+    return (
+      <div className="Coffee-Form">
+        {/* <h1>Submit New Coffee</h1>
+        <form>
+          <p>
+            <label htmlFor="title">Coffee Name:</label>
+            <input type="text" id="title" name="title" value={this.props.formValue.title}></input>
+          </p>
+          <p>
+            <label htmlFor="description">Description:</label>
+            <input
+              type="text"
+              id="description"
+              name="description"
+              value={this.props.formValue.description}
+            ></input>
+          </p>
+          <p>
+            <label htmlFor="tasted">Tasted?:</label>
+            <input
+              type="checkbox"
+              id="tasted"
+              name="tasted"
+              checked={this.props.formValue.tasted}
+            ></input>
+          </p>
+        </form> */}
+      </div>
+    );
   }
 }
 
 class Coffee extends React.Component {
+  handleClick = key => {
+    console.log('big gulp');
+    this.props.drinkCoffee();
+  };
+
   render() {
     return (
-      <div className="coffee" key={this.props.coffee.key}>
+      <div className="coffee">
         <h3>{this.props.coffee.title}</h3>
-        <p>Tasted? {this.props.coffee.tasted ? 'Yes!' : 'Not yet!'}</p>
+        <p onClick={this.handleClick}>Tasted? {this.props.coffee.tasted ? 'Yes!' : 'Not yet!'}</p>
       </div>
     );
   }
@@ -42,12 +75,11 @@ class Coffee extends React.Component {
 
 class CoffeeList extends React.Component {
   render() {
-    // render all coffees
     return (
       <div>
         <h1>Coffee List</h1>
         {this.props.coffees.map(coffee => {
-          return <Coffee coffee={coffee} />;
+          return <Coffee coffee={coffee} key={coffee.key} />;
         })}
       </div>
     );
@@ -59,24 +91,45 @@ class App extends React.Component {
     super(props);
     this.state = {
       coffees: [...defaultCoffees],
-      formValue: '',
+      formValue: {
+        title: '',
+        description: '',
+        tasted: false
+      },
       searchText: ''
     };
   }
 
-  drinkCoffee() {
-    // makes change to state that coffee is drank/undrank
-  }
+  drinkCoffee = () => {
+    console.log('gulp');
+    // this.setState({
+    //   coffees: this.state.coffees.map(coffee => {
+    //     if (coffee.key === coffeeKey) {
+    //       coffee.tasted = !coffee.tasted;
+    //     }
+    //     return coffee;
+    //   })
+    // });
+
+    // // copy current list of coffees
+    // let coffees = this.state.coffees;
+    // // find the current coffee in the list
+    // let currentCoffee = coffees.find(obj => obj.key === coffeeKey);
+    // // make the change
+    // currentCoffee.tasted = !currentCoffee.tasted;
+    // // reset state to replace with this list
+    // this.setState({ coffees: coffees });
+  };
 
   render() {
     return (
       <div className="coffee-journal">
         <h1>Coffee Journal</h1>
         <div className="coffee-list">
-          <CoffeeList coffees={this.state.coffees} />
+          <CoffeeList coffees={this.state.coffees} drinkCoffee={this.drinkCoffee} />
         </div>
         <div className="coffee-form">
-          <NewCoffeeForm />
+          <NewCoffeeForm formValue={this.state.formValue} />
         </div>
       </div>
     );
